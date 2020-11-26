a459e37b1b22853611892b5cffc258e5f241a4cf
bryang229 <60994504+bryang229@users.noreply.github.com>
2020-11-26 17:13:12 -0500

    Improved documentation
    I fine tuned the radar code to work with an Arduino Mega, I also redid the wiring so it looked better so refrence to the image would be improved

--- radar.ino
+++ radar.ino
@@ -1,20 +1,33 @@
+#include <SR04.h>
 #include "LedControl.h"
-#include "SR04.h"
-#define TRIG_PIN1 9
+#define TRIG_PIN1 7
 #define ECHO_PIN1 8
-#define TRIG_PIN2 7
-#define ECHO_PIN2 6
-#define TRIG_PIN3 5
-#define ECHO_PIN3 4
-#define TRIG_PIN4 3
-#define ECHO_PIN4 2
+#define TRIG_PIN2 3
+#define ECHO_PIN2 2
+#define TRIG_PIN3 51
+#define ECHO_PIN3 53
+#define TRIG_PIN4 50
+#define ECHO_PIN4 48
+#define rxd 69
+#define txd 35
+#define BUFFER_SIZE 64
+
+const int alertDistance = 25;
+
+const int  power [4] = {
+  6,
+  4,
+  49,
+  52
+};
 
 SR04 sr04a = SR04(ECHO_PIN1, TRIG_PIN1);
 SR04 sr04b = SR04(ECHO_PIN2, TRIG_PIN2);
 SR04 sr04c = SR04(ECHO_PIN3, TRIG_PIN3);
 SR04 sr04d = SR04(ECHO_PIN4, TRIG_PIN4);
 
-LedControl lc = LedControl(12, 10, 11, 1);
+LedControl lc = LedControl(13, 11, 12, 1);
+// (datain, clk, cs(load))
 
 byte rows[5] = {
   B00000000,
@@ -25,11 +38,15 @@ byte rows[5] = {
 };
 
 void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(9, OUTPUT);
-  digitalWrite(13, HIGH);
-  digitalWrite(9, HIGH);
-  lc.shutdown(0, false);
+  pinMode(power[0],OUTPUT);
+  pinMode(power[1],OUTPUT);
+  pinMode(power[2],OUTPUT);
+  pinMode(power[3],OUTPUT);
+  digitalWrite(power[0],HIGH);
+  digitalWrite(power[1],HIGH);
+  digitalWrite(power[2],HIGH);
+  digitalWrite(power[3],HIGH);
+   lc.shutdown(0, false);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
   Serial.begin(9600);
@@ -48,8 +65,8 @@ void logic() {
   //c is right
   //d is bottom
 
-  if (a < 100) {
-    if (c < 100) {
+  if (a < alertDistance) {
+    if (c < alertDistance) {
       lc.setRow(0, 3, rows[4]);
       lc.setRow(0, 4, rows[4]);
     }
@@ -58,7 +75,7 @@ void logic() {
       lc.setRow(0, 4, rows[3]);
     }
   }
-  else if (c < 100) {
+  else if (c < alertDistance) {
     lc.setRow(0, 3, rows[2]);
     lc.setRow(0, 4, rows[2]);
   }
@@ -67,7 +84,7 @@ void logic() {
     lc.setRow(0, 4, rows[1]);
   }
 
-  if (b < 100) {
+  if (b < alertDistance) {
     lc.setRow(0, 0, rows[1]);
     lc.setRow(0, 1, rows[1]);
   }
@@ -75,7 +92,7 @@ void logic() {
     lc.setRow(0, 0, rows[0]);
     lc.setRow(0, 1, rows[0]);
   }
-  if (d < 100) {
+  if (d < alertDistance) {
     lc.setRow(0, 6, rows[1]);
     lc.setRow(0, 7, rows[1]);
   }
@@ -86,6 +103,6 @@ void logic() {
 
 }
 
-void loop() {
+void loop() {    
   logic();
 }
